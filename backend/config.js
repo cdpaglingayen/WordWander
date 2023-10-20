b7b0367dc8d86c4b78be7e42a39b1314bc51f7ff
Charles Danielle Paglingayen <65948329+cdpaglingayen@users.noreply.github.com>
2023-10-20 21:07:18 +0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
 export const PORT = 5555
 
-export const mongoDBURL = ''
+export const mongoDBURL =
+  'mongodb+srv://charlesdanius:cACjNNVO8h8zV9N8@nodeexpressprojects.bnejrow.mongodb.net/jobs-api?retryWrites=true&w=majority'
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
